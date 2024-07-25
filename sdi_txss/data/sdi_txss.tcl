67911a190921ee5ff3e1c13a47318b21c9c544e9
Katta Dhanunjanrao <katta.dhanunjanrao@amd.com>
2024-07-25 03:48:07 -1200

    sdi_txss: Add support for linux sdt flow
    Add the support for SDI TxSS to linux SDT flow.
    remote port creation and missing proprties fix.
    
    Signed-off-by: Katta Dhanunjanrao <katta.dhanunjanrao@amd.com>
    Acked-by: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- sdi_txss/data/sdi_txss.tcl
+++ sdi_txss/data/sdi_txss.tcl
@@ -41,9 +41,98 @@ proc sdi_txss_generate {drv_handle} {
 	set Isstd_352 [hsi get_property CONFIG.C_TX_INSERT_C_STR_ST352 [hsi get_cells -hier $drv_handle]]
 	if {$Isstd_352 == "flase"} {
 		add_prop "${node}" "xlnx,Isstd_352" 0 int $dts_file 1
+		add_prop "${node}" "xlnx,tx-insert-c-str-st352" false int $dts_file 1
 	} else {
 		add_prop "${node}" "xlnx,Isstd_352" 1 int $dts_file 1
+		add_prop "${node}" "xlnx,tx-insert-c-str-st352" true int $dts_file 1
 	}
+
+
+	set ports_node [create_node -n "ports" -l sditx_ports$drv_handle -p $node -d $dts_file]
+	add_prop "$ports_node" "#address-cells" 1 int $dts_file 1
+	add_prop "$ports_node" "#size-cells" 0 int $dts_file 1
+	set sdi_port_node [create_node -n "port" -l encoder_sdi_port$drv_handle -u 0 -p $ports_node -d $dts_file]
+	add_prop "$sdi_port_node" "reg" 0 int $dts_file 1
+	set sditx_in_ip [get_connected_stream_ip [hsi::get_cells -hier $drv_handle] "VIDEO_IN"]
+	if {![llength $sditx_in_ip]} {
+		dtg_warning "$drv_handle pin VIDEO_IN is not connected...check your design"
+	}
+	set inip ""
+	foreach inip $sditx_in_ip {
+		if {[llength $inip]} {
+			set master_intf [hsi get_intf_pins -of_objects [hsi get_cells -hier $inip] -filter {TYPE==SLAVE || TYPE ==TARGET}]
+			set ip_mem_handles [hsi get_mem_ranges $inip]
+			if {[llength $ip_mem_handles]} {
+				set base [string tolower [hsi::get_property BASE_VALUE $ip_mem_handles]]
+				if {[string match -nocase [hsi::get_property IP_NAME $inip] "v_frmbuf_rd"]} {
+					gen_frmbuf_rd_node $inip $drv_handle $sdi_port_node $dts_file
+				}
+			} else {
+				if {[string match -nocase [hsi::get_property IP_NAME $inip] "system_ila"]} {
+					continue
+				}
+				set inip [get_in_connect_ip $inip $master_intf]
+				if {[string match -nocase [hsi::get_property IP_NAME $inip] "v_frmbuf_rd"]} {
+					gen_frmbuf_rd_node $inip $drv_handle $sdi_port_node $dts_file
+				}
+			}
+		}
+	}
+	if {[llength $inip]} {
+		set sditx_in_end ""
+		set sditx_remo_in_end ""
+		if {[info exists end_mappings] && [dict exists $end_mappings $inip]} {
+			set sditx_in_end [dict get $end_mappings $inip]
+			dtg_verbose "sditx_in_end:$sditx_in_end"
+		}
+		if {[info exists remo_mappings] && [dict exists $remo_mappings $inip]} {
+			set sditx_remo_in_end [dict get $remo_mappings $inip]
+			dtg_verbose "sditx_remo_in_end:$sditx_remo_in_end"
+		}
+		if {[llength $sditx_remo_in_end]} {
+			set sditx_node [create_node -n "endpoint" -l $sditx_remo_in_end -p $sdi_port_node -d $dts_file]
+		}
+		if {[llength $sditx_in_end]} {
+			add_prop "$sditx_node" "remote-endpoint" $sditx_in_end reference $dts_file 1
+		}
+	}
+
+	set audio_connected_ip [get_connected_stream_ip [hsi::get_cells -hier $drv_handle] "SDI_TX_ANC_DS_OUT"]
+        if {[llength $audio_connected_ip] != 0} {
+                set audio_connected_ip_type [hsi::get_property IP_NAME $audio_connected_ip]
+                if {[string match -nocase $audio_connected_ip_type "v_uhdsdi_audio"]} {
+                        set sdi_audio_port [create_node -n "port" -l sdi_audio_port -u 1 -p $ports_node -d $dts_file]
+                        add_prop "$sdi_audio_port" "reg" 1 int $dts_file 1
+                        set sdi_audio_node [create_node -n "endpoint" -l sdi_audio_sink_port -p $sdi_audio_port -d $dts_file]
+                        add_prop "$sdi_audio_node" "remote-endpoint" sditx_audio_embed_src reference $dts_file 1
+                }
+        } else {
+                dtg_warning "$drv_handle:connected ip for audio port pin SDI_TX_ANC_DS_OUT is NULL"
+        }
+
+}
+proc gen_frmbuf_rd_node {ip drv_handle hdmi_port_node dts_file} {
+	set frmbuf_rd_node [create_node -n "endpoint" -l encoder$drv_handle -p $hdmi_port_node -d $dts_file]
+	add_prop "$frmbuf_rd_node" "remote-endpoint" $ip$drv_handle reference $dts_file 1
+	global env
+	set path $env(REPO)
+	set common_file "$path/device_tree/data/config.yaml"
+	set dt_overlay [get_user_config $common_file -dt_overlay]
+	if {$dt_overlay} {
+		set bus_node "amba"
+	} else {
+		set bus_node "amba_pl: amba_pl"
+	}
+	set pl_display [create_node -n "drm-pl-disp-drv$drv_handle" -l "v_pl_disp$drv_handle" -p $bus_node -d $dts_file]
+	add_prop $pl_display "compatible" "xlnx,pl-disp" string $dts_file 1
+	add_prop $pl_display "dmas" "$ip 0" reference $dts_file 1
+	add_prop $pl_display "dma-names" "dma0" string $dts_file 1
+#	add_prop "${pl_display}" "/* Fill the field xlnx,vformat based on user requirement */" "" comment
+	add_prop $pl_display "xlnx,vformat" "YUYV" string $dts_file 1
+	set pl_display_port_node [create_node -n "port" -l pl_display_port$drv_handle -u 0 -p $pl_display -d $dts_file]
+	add_prop "$pl_display_port_node" "reg" 0 int $dts_file 1
+	set pl_disp_crtc_node [create_node -n "endpoint" -l $ip$drv_handle -p $pl_display_port_node -d $dts_file]
+	add_prop "$pl_disp_crtc_node" "remote-endpoint" encoder$drv_handle reference $dts_file 1
 }
 
 proc sdi_tx_add_hier_instances {drv_handle} {
