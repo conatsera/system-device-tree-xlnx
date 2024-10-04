ad5e76e2addd514334f9a98b8cee595ff66f3e9c
Mahapatra, Amit Kumar <amit.kumar-mahapatra@amd.com>
2024-10-03 18:15:09 -1200

    Remove reg property from nor0 subnode
    nor0 is a subnode of smcc and doesn't require reg property.
    Update the tcl to remove reg property from nor0 subnode
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- norps/data/norps.tcl
+++ norps/data/norps.tcl
@@ -16,8 +16,6 @@
     proc norps_generate {drv_handle} {
       
         # TODO: if addr25 is used, should we consider set the reg size to 64MB?
-        # enable reg generation for ps ip
-        gen_reg_property $drv_handle "enable_ps_ip"
     }
 
 
