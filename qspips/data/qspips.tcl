74c42d0f47d853265454fbe69aed10f76470aaee
Onkar Harsh <onkar.harsh@amd.com>
2024-10-23 16:54:05 +0530

    qspips: Sync DTG code to SDT
    Commit:
    2e6986be72f95f79e3fdc596cf6786caed7a7bf7
    qspips: Set num-cs based on qspi mode
    Remove static num-cs value from mdd file and update it as per the qspi
    mode.
    If qspi mode is dual or stacked set num-cs to 2 else 1.
    
    33fa43d8e4b5b57a2ad75264f22fe5a7d6afe0a6
    qspips.tcl: set spi-max-frequency to 40MHZ
    set the qspi max-frequency to 40MHZ when feedback clock
    diabled in versalnet designs.
    
    Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
    Signed-off-by: Varalaxmi Bingi <varalaxmi.bingi@amd.com>
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>

--- qspips/data/qspips.tcl
+++ qspips/data/qspips.tcl
@@ -18,7 +18,8 @@
         set dts_file [set_drv_def_dts $drv_handle]
         set slave [hsi::get_cells -hier $drv_handle]
         set qspi_mode [get_ip_param_value $slave "C_QSPI_MODE"]
-        set ipname [hsi get_cells -hier -filter {IP_NAME == "pspmc" || IP_NAME == "pmcps" || IP_NAME == "psx_wizard" || IP_NAME == "ps_wizard"}]
+
+        set ipname [hsi get_cells -hier -filter {IP_NAME == "pspmc" || IP_NAME == "pmcps" || IP_NAME == "ps_wizard"}]
         if {[llength $ipname]} {
             set fbclk [hsi get_property CONFIG.PMC_QSPI_FBCLK [hsi get_cells -hier $ipname]]
                    if {[regexp "ENABLE 0" $fbclk matched]} {
@@ -26,8 +27,7 @@
                        if {$node == 0} {
                             return
                        }
-                       add_prop "${node}" "//* hw design is missing feedback clock that's why spi-max-frequency is 40MHz */" "" comment $dts_file
-                       add_prop $node spi-max-frequency 40000000 int $dts_file
+                       qspips_set_spi_max_frequency $node $dts_file
                        add_prop $node qspi-fbclk 0 int $dts_file
                    } elseif {[regexp "ENABLE 1" $fbclk matched]} {
                        set node [gen_peripheral_nodes $drv_handle]
@@ -39,6 +39,32 @@
 	} else {
                        add_prop $node qspi-fbclk 0 int $dts_file
 	}
+
+
+        #for versal-net setting spi-max-frequency to 40MHZ if fbclk disabled in design
+        set psxwizard [hsi get_cells -hier -filter {IP_NAME == "psx_wizard"}]
+        if {[llength $psxwizard]} {
+                set psx_pmcx_config [hsi get_property CONFIG.PSX_PMCX_CONFIG [hsi get_cells -hier $psxwizard]]
+                if {[llength $psx_pmcx_config]} {
+                        set qspi_fbclk ""
+                        if {[dict exists $psx_pmcx_config "PMCX_QSPI_FBCLK"]} {
+                                set qspi_fbclk [dict get $psx_pmcx_config "PMCX_QSPI_FBCLK"]
+                                set qspi_fbclk_enabled ""
+                                if {[dict exists $qspi_fbclk "ENABLE"]} {
+                                        set qspi_fbclk_enabled [dict get $qspi_fbclk "ENABLE"]
+                                        if {$qspi_fbclk_enabled == 0} {
+                                                qspips_set_spi_max_frequency $node $dts_file
+                                        }
+                                }
+                        }
+                }
+        } else {
+               add_prop $node qspi-fbclk 0 int $dts_file
+        }
+
+
+
+
         set is_stacked 0
         if { $qspi_mode == 2} {
                 set is_dual 1
@@ -49,6 +75,14 @@
                 set is_dual 0
         }
         add_prop $node "is-dual" $is_dual int $dts_file
+
+        # Set num-cs value to 2 if qspi is dual/stacked else 1
+        set numcs 1
+        if { $is_dual == 1 || $is_stacked == 1 } {
+                set numcs 2
+        }
+        add_prop $node "num-cs" $numcs int $dts_file
+
         if {$is_stacked} {
                 add_prop $node "is-stacked" $is_stacked int $dts_file
         }
@@ -82,3 +116,9 @@
     }
 
 
+#when fbclk disabled in the design
+#this function will set the spi-max-freq to 40MHZ
+proc qspips_set_spi_max_frequency {node dts_file} {
+        add_prop "${node}" "//* hw design is missing feedback clock that's why spi-max-frequency is 40MHz */" "" comment $dts_file
+        add_prop $node "spi-max-frequency" 40000000 int $dts_file
+}
