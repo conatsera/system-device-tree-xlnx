3bf7dd0b969a3dfe47a299325404cc69c3e21b68
Onkar Harsh <onkar.harsh@amd.com>
2024-10-23 08:35:10 +0530

    vtc: DTG sync to SDT
    Commit:
    cb9bd4f6e0741fe6cae6997a80077d1c08529cc2
    mixer:vtc: Add video format and vtc compatible string
    Add video format type for the mixer layer-0 and update vtc compatible
    string to xlnx,bridge-v-tc-6.1.
    
    Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>

--- vtc/data/vtc.tcl
+++ vtc/data/vtc.tcl
@@ -20,6 +20,7 @@
         }
         set dts_file [set_drv_def_dts $drv_handle]
         pldt append $node compatible "\ \, \"xlnx,v-tc-6.1\""
+        pldt append $node compatible "\ \, \"xlnx,bridge-v-tc-6.1\""
         set generate_en [hsi get_property CONFIG.C_GENERATE_EN [hsi::get_cells -hier $drv_handle]]
         if {$generate_en == 1} {
                 add_prop "${node}" "xlnx,generator" boolean $dts_file
