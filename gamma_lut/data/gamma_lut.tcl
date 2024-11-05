0d3d18eb57b16d48a2fccab49509bab73a6578ca
Kunal Rane <kunal.rane@amd.com>
2024-11-04 14:10:24 -1200

    gamma_lut: data: Add no axi slice logic
    Insert reset-gpio property in DT node when AXI slice is not used
    between AXI GPIO and gamma_lut IP.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- gamma_lut/data/gamma_lut.tcl
+++ gamma_lut/data/gamma_lut.tcl
@@ -1,6 +1,6 @@
 #
 # (C) Copyright 2018-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -165,6 +165,12 @@
                         }
                     }
                 }
+			# add reset-gpio pin when no slice is connected between v_tpg ip and axi_gpio ip
+			set ip_name [hsi::get_property IP_NAME $sink_periph]
+			if {[string match -nocase $ip_name "axi_gpio"]} {
+				set gpio_number [hsi::get_property LEFT [hsi::get_pins -of_objects [hsi::get_cells -hier "$sink_periph"] "gpio_io_o" ]]
+				add_prop "$node" "reset-gpios" "$sink_periph $gpio_number 1" reference $dts_file
+			}
             } else {
                 dtg_warning "$drv_handle: peripheral is NULL for the $pin $sink_periph"
             }
