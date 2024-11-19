conatsera/system-device-tree-xlnx

commit c1f3dd849071a639442190c699021af38cc41011

device_tree: data: Remove dt_overlay flag and relevant changes
Remove the dt_overlay flag and associated changes, as this flag
is not applicable for the SDT flow.

Signed-off-by: Nava kishore Manne <nava.kishore.manne@amd.com>
Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

device_tree/data/common_proc.tcl
@@ -440,7 +440,6 @@ proc get_user_config args {
 	        set config_file [lindex $args 0]
        		set cfg [get_yaml_dict $config_file]
 	        set user [dict get $cfg dict_devicetree]
-	        set overlay [dict get $user dt_overlay]
         	set mainline_kernel [dict get $user mainline_kernel]
         	set kernel_ver [dict get $user kernel_ver]
         	set dir [dict get $user output_dir]
@@ -455,8 +454,6 @@ proc get_user_config args {
                         	set param $config_dts
                 	} -board_dts {
                         	set param ""
-                	} -dt_overlay {
-                        	set param $overlay
                 	} -pl_only {
                         	set param $pl_only
 			} -mainline_kernel {
@@ -1365,7 +1362,6 @@ proc write_dt args {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
 	# Windows treats an empty env variable as not defined
 	if {[catch {set board_dtsi_file $env(board)} msg]} {
 		set board_dtsi_file ""
@@ -1381,9 +1377,6 @@ proc write_dt args {
 			}
 			puts $fd "#include \"$val\""
 		}
-	}
-	if {[string match -nocase $dt "pldt"] && $dt_overlay} {
-		puts $fd "\/dts-v1\/\n\/plugin\/;"
 	}
 	set dtcheck [string match -nocase $dt "pcwdt"]
 	if {$dtcheck != 1} {
@@ -2670,31 +2663,6 @@ proc list_remove_element {cur_list elements} {
 	return $cur_list
 }
 
-proc update_overlay_custom_dts_include {include_file} {
-	set dt_overlay [hsi get_property CONFIG.dt_overlay [get_os]]
-	set overlay_custom_dts [hsi get_property CONFIG.overlay_custom_dts [get_os]]
-	set overlay_custom_dts_obj [get_dt_trees ${overlay_custom_dts}]
-	if {[string_is_empty $overlay_custom_dts_obj] == 1} {
-		set overlay_custom_dts_obj [set_cur_working_dts ${overlay_custom_dts}]
-	}
-	if {[string equal ${include_file} ${overlay_custom_dts_obj}]} {
-		return 0
-	}
-	set cur_inc_list [hsi get_property INCLUDE_FILES $overlay_custom_dts_obj]
-	set tmp_list [split $cur_inc_list ","]
-	if { [lsearch $tmp_list $include_file] < 0} {
-		if {[string_is_empty $cur_inc_list]} {
-			set cur_inc_list $include_file
-		} else {
-			append cur_inc_list "," $include_file
-			set field [split $cur_inc_list ","]
-			set cur_inc_list [lsort -decreasing $field]
-			set cur_inc_list [join $cur_inc_list ","]
-		}
-		set_property INCLUDE_FILES ${cur_inc_list} $overlay_custom_dts_obj
-	}
-}
-
 proc update_system_dts_include {include_file} {
 	# where should we get master_dts data
 	global count
@@ -2760,39 +2728,18 @@ proc set_drv_def_dts {drv_handle} {
 	#set drvname [get_drivers $drv_handle]
 	#set common_file "$path/device_tree/data/config.yaml"
 	set common_file [file join [file dirname [dict get [info frame 0] file]] "config.yaml"]
-	set dt_overlay [get_user_config $common_file -dt_overlay]
 	set family [get_hw_family]
 	global bus_clk_list
 	set pl_ip [is_pl_ip $drv_handle]
 	if {$pl_ip} {
 		set default_dts "pl.dtsi"
-		if {!$dt_overlay} {
-			update_system_dts_include $default_dts
-		}
+		update_system_dts_include $default_dts
 	} else {
 		# PS IP, read pcw_dts property
 		set default_dts "pcw.dtsi"
 		update_system_dts_include $default_dts
 	}
-	if {$pl_ip && $dt_overlay} {
-		set fpga_node [create_node -n "fragment" -u 0 -d ${default_dts} -p root]
-		set pl_file $default_dts
-		set targets "fpga_full"
-		add_prop $fpga_node target "$targets" reference $default_dts 1
-		set child_name "__overlay__"
-		set child_node [create_node -l "overlay0" -n $child_name -p $fpga_node -d $default_dts]
-		add_prop "${child_node}" "#address-cells" 2 int $default_dts 1
-		add_prop "${child_node}" "#size-cells" 2 int $default_dts 1
-		if {[is_zynqmp_platform $family]} {
-			set hw_name [::hsi::get_hw_files -filter "TYPE == bit"]
-			add_prop "${child_node}" "firmware-name" "$hw_name.bin" string  $default_dts 1
-			add_prop "root" "firmware-name" "$hw_name" string  $default_dts 1
-		} elseif {[string match -nocase $family "versal"]} {
-			set hw_name [::hsi::get_hw_files -filter "TYPE == pdi"]
-			add_prop "${child_node}" "firmware-name" "$hw_name" string  $default_dts 1
-			add_prop "root" "firmware-name" "$hw_name" string  $default_dts 1
-		}
-	}
+
 	return $default_dts
 }
 
@@ -6241,12 +6188,7 @@ proc gen_peripheral_nodes {drv_handle {node_only ""}} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-        if {$dt_overlay} {
-                set ignore_list "PERIPHERAL axi_noc dfx_decoupler mig_7series"
-        } else {
-                set ignore_list "PERIPHERAL axi_noc mig_7series"
-        }
+	set ignore_list "lmb_bram_if_cntlr PERIPHERAL axi_noc mig_7series"
 	if {[string match -nocase $ip_type "psu_pcie"]} {
 		set pcie_config [hsi get_property CONFIG.C_PCIE_MODE [hsi::get_cells -hier $drv_handle]]
 		if {[string match -nocase $pcie_config "Endpoint Device"]} {
@@ -6455,17 +6397,6 @@ proc detect_bus_name {ip_drv} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-		if {[is_pl_ip $ip_drv] && $dt_overlay} {
-			# create the parent_node for pl.dtsi
-			set default_dts [set_drv_def_dts $ip_drv]
-			set fpga_node [create_node -n "fragment" -u 2 -d $default_dts -p root]
-			set targets "amba"
-			add_prop $fpga_node target "$targets" reference $default_dts 1
-			set child_name "__overlay__"
-			set bus_node [create_node -l "overlay2" -n $child_name -p $fpga_node -d $default_dts]
-			return "overlay2: __overlay__"
-		}
 		if {[is_pl_ip $ip_drv]}  {
 			# create the parent_node for pl.dtsi
 			set default_dts [set_drv_def_dts $ip_drv]
@@ -6567,38 +6498,21 @@ proc add_or_get_bus_node {ip_drv dts_file} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
 	set proctype [get_hw_family]
-	if {[is_pl_ip $ip_drv] && $dt_overlay} {
-		set dts "pl.dtsi"
-		set fpga_node [create_node -n "fragment" -u 2 -d $dts -p root]
-		set targets "amba"
-		add_prop $fpga_node target "$targets" reference $dts 1
-		set child_name "__overlay__"
-		set bus_node [create_node -l "overlay2" -n $child_name -p $fpga_node -d $dts]
+	set bus_node $bus_name
+	if {[string match -nocase $bus_node "amba_pl: amba_pl"]} {
 		if {[is_zynqmp_platform $proctype] || [string match -nocase $proctype "versal"] || $is_64_bit_mb} {
-			add_prop "${bus_node}" "#address-cells" 2 int $dts 1
-			add_prop "${bus_node}" "#size-cells" 2 int $dts 1
+			set addr_cells 2
+			set size_cells 2
 		} else {
-			add_prop "${bus_node}" "#address-cells" 1 int $dts 1
-			add_prop "${bus_node}" "#size-cells" 1 int $dts 1
+			set addr_cells 1
+			set size_cells 1
 		}
-	} else {
-		set bus_node $bus_name
-		if {[string match -nocase $bus_node "amba_pl: amba_pl"]} {
-			if {[is_zynqmp_platform $proctype] || [string match -nocase $proctype "versal"] || $is_64_bit_mb} {
-				set addr_cells 2
-				set size_cells 2
-			} else {
-				set addr_cells 1
-				set size_cells 1
-			}
-			if {[catch {set val [pldt get $bus_node #address-cells]} msg]} {
-				add_prop $bus_node #address-cells $addr_cells int $dts_file
-				add_prop $bus_node #size-cells $size_cells int $dts_file
-				add_prop $bus_node compatible "simple-bus" string $dts_file
-				add_prop $bus_node ranges boolean $dts_file
-			}
+		if {[catch {set val [pldt get $bus_node #address-cells]} msg]} {
+			add_prop $bus_node #address-cells $addr_cells int $dts_file
+			add_prop $bus_node #size-cells $size_cells int $dts_file
+			add_prop $bus_node compatible "simple-bus" string $dts_file
+			add_prop $bus_node ranges boolean $dts_file
 		}
 	}
 	return $bus_node

ai_engine/data/ai_engine.tcl
@@ -106,19 +106,8 @@
         if {$node == 0} {
                 return
         }
-        set dt_overlay 0
-        #set dt_overlay [hsi get_property CONFIG.dt_overlay [get_os]]
-        if {$dt_overlay} {
-                set RpRm [get_rp_rm_for_drv $drv_handle]
-                regsub -all { } $RpRm "" RpRm
-                if {[llength $RpRm]} {
-                        set bus_node "overlay2_$RpRm"
-                } else  {
-                        set bus_node "overlay2"
-                }
-        } else {
-                set bus_node "amba_pl: amba_pl"
-        }
+
+        set bus_node "amba_pl: amba_pl"
         ai_engine_generate_aie_array_device_info ${node} ${drv_handle} ${bus_node}
         set ip [hsi get_cells -hier $drv_handle]
         set unit_addr [get_baseaddr ${ip} no_prefix]

axi_ethernet/data/axi_ethernet.tcl
@@ -3,7 +3,7 @@
 # Based on original code:
 # (C) Copyright 2007-2014 Michal Simek
 # (C) Copyright 2014-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # Michal SIMEK <monstr@monstr.eu>
 #
@@ -24,7 +24,6 @@
         global dtsi_fname
         set path $env(REPO)
         set common_file "$path/device_tree/data/config.yaml"
-        set dt_overlay [get_user_config $common_file -dt_overlay]
         set bus_node [detect_bus_name $drv_handle]
 
         set node [get_node $drv_handle]
@@ -72,11 +71,7 @@
         set eth_node ""
         for {set core 0} {$core < $num_cores} {incr core} {
                 if {$ip_name == "xxv_ethernet"  && $core != 0} {
-                    if {$dt_overlay} {
-                         set bus_node "overlay2"
-                    } else {
-                        set bus_node "amba_pl: amba_pl"
-                    }
+                    set bus_node "amba_pl: amba_pl"
                     set dts_file "pl.dtsi"
                 set ipmem_len [llength $ip_mem_handles]
                 if {$ipmem_len > 1} {

device_tree/data/config.yaml
@@ -17,7 +17,6 @@ dict_devicetree:
     kernel_ver: 2024.2
     mainline_kernel: none
     board_dts: versal-vck190-reva
-    dt_overlay: false
     output_dir: output_dts
     zocl: disable
 ---

device_tree/data/device_tree.tcl
@@ -362,7 +362,6 @@ proc set_dt_param args {
                                 -xsa {set env(xsa) [Pop args 1]}
 				-rm_xsa {set env(rm_xsa) [Pop args 1]}
                                 -board_dts {set env(board) [Pop args 1]}
-                                -dt_overlay {set env(dt_overlay) [Pop args 1]}
                                 -mainline_kernel {set env(kernel) [Pop args 1] }
                                 -kernel_ver {set env(kernel_ver) [Pop args 1]}
                                 -dir {set env(dir) [Pop args 1]}
@@ -400,8 +399,6 @@ proc get_dt_param args {
                } -board -
                  -board_dts {
                        if {[catch {set val $env(board)} msg ]} {}
-               } -dt_overlay {
-                       if {[catch {set val $env(dt_overlay)} msg ]} {}
                } -mainline_kernel {
                        if {[catch {set val $env(kernel)} msg ]} {}
                } -kernel_ver {
@@ -2484,11 +2481,7 @@ proc gen_xppu {drv_handle} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
 	set ip [hsi get_property IP_NAME [hsi::get_cells -hier $drv_handle]]
-	if {[string match -nocase $ip "axi_noc"] && $dt_overlay} {
-		return
-	}
 	set node [get_node $drv_handle]
 	set baseaddr [get_baseaddr $drv_handle noprefix]
 	set xppu [dict create]
@@ -3913,11 +3906,7 @@ proc gen_power_domains {drv_handle} {
 
         set path $env(REPO)
         set common_file "$path/device_tree/data/config.yaml"
-        set dt_overlay [get_user_config $common_file -dt_overlay]
         set ip [hsi get_property IP_NAME [hsi::get_cells -hier $drv_handle]]
-        if {[string match -nocase $ip "axi_noc"] && $dt_overlay} {
-                return
-        }
         set node [get_node $drv_handle]
         set baseaddr [get_baseaddr $drv_handle noprefix]
         set node_id [dict create]

dp_rxss14/data/dp_rxss14.tcl
@@ -158,13 +158,7 @@ proc gen_frmbuf_wr_node {outip drv_handle port0_node dtsi_file} {
 	set path $env(REPO)
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-
-	if {$dt_overlay} {
-		set bus_node "amba"
-	} else {
-		set bus_node "amba_pl: amba_pl"
-	}
+	set bus_node "amba_pl: amba_pl"
         set bus_node [detect_bus_name $drv_handle]
         set vcap [create_node -n "vcap_dprx$drv_handle" -p $bus_node -d $dtsi_file]
         add_prop $vcap "compatible" "xlnx,video" string $dtsi_file
@@ -229,12 +223,7 @@ proc gen_xfmc_node {drv_handle dts_file} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-        if {$dt_overlay} {
-                set bus_node "amba"
-        } else {
-                set bus_node "amba_pl: amba_pl"
-        }
+	set bus_node "amba_pl: amba_pl"
         set pl_disp [create_node -n "xv_fmc$drv_handle" -l "xfmc$drv_handle" -p $bus_node -d $dts_file]
         add_prop $pl_disp "compatible" "xilinx-vfmc" string $dts_file 1
 }

dp_txss14/data/dp_txss14.tcl
@@ -176,12 +176,7 @@ proc gen_frmbuf_rd_node {ip drv_handle port0_node dtsi_file} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-        if {$dt_overlay} {
-                set bus_node "amba"
-        } else {
-                set bus_node "amba_pl: amba_pl"
-        }
+	set bus_node "amba_pl: amba_pl"
         set pl_disp [create_node -n "drm-pl-disp-drv" -l "v_pl_disp$drv_handle" -p $bus_node -d $dtsi_file]
         add_prop $pl_disp "compatible" "xlnx,pl-disp" string $dtsi_file 1
 	add_prop $pl_disp "dmas" "$ip 0" reference $dtsi_file 1
@@ -272,12 +267,7 @@ proc gen_xfmc_node {drv_handle dts_file} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-        if {$dt_overlay} {
-                set bus_node "amba"
-        } else {
-                set bus_node "amba_pl: amba_pl"
-        }
+	set bus_node "amba_pl: amba_pl"
         set pl_disp [create_node -n "xv_fmc$drv_handle" -l "xfmc$drv_handle" -p $bus_node -d $dts_file]
         add_prop $pl_disp "compatible" "xilinx-vfmc" string $dts_file 1
 }

hdmi_rx_ss/data/hdmi_rx_ss.tcl
@@ -1,6 +1,6 @@
 #
 # (C) Copyright 2018-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -160,13 +160,7 @@ proc gen_frmbuf_node {ip drv_handle dts_file} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-#	set dt_overlay [hsi::get_property CONFIG.dt_overlay [get_os]]
-	set dt_overlay [get_user_config $common_file -dt_overlay]
-        if {$dt_overlay} {
-                set bus_node "amba"
-        } else {
-               set bus_node "amba_pl: amba_pl"
-        }
+	set bus_node "amba_pl: amba_pl"
         set vcap [create_node -n "vcap_$drv_handle" -p $bus_node -d $dts_file]
         add_prop $vcap "compatible" "xlnx,video" string $dts_file
         add_prop $vcap "dmas" "$ip 0" reference $dts_file

commit f3745b5a86f6bbe5fd572c62ef922a97528e05d9

common_proc: Remove the lmb ram from the ignorelist
If the lmb ram is in the ignore list then the driver is not pulled.
Remove it from the ignore list.

Fixes: c1f3dd84 ("device_tree: data: Remove dt_overlay flag and relevant changes")
Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
Acked-by: Srinivas Goud <srinivas.goud@amd.com>

device_tree/data/common_proc.tcl
@@ -6216,7 +6216,7 @@ proc gen_peripheral_nodes {drv_handle {node_only ""}} {
 	global env
 	set path $env(REPO)
 	set common_file "$path/device_tree/data/config.yaml"
-	set ignore_list "lmb_bram_if_cntlr PERIPHERAL axi_noc mig_7series"
+	set ignore_list "PERIPHERAL axi_noc mig_7series"
 	if {[string match -nocase $ip_type "psu_pcie"]} {
 		set pcie_config [hsi get_property CONFIG.C_PCIE_MODE [hsi::get_cells -hier $drv_handle]]
 		if {[string match -nocase $pcie_config "Endpoint Device"]} {