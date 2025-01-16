a70ccef17a887c9ddb9327d9011e09ad56560750
Arshad Khan <arshad.khan@amd.com>
2025-01-15 21:01:44 -1200

    usbpsu: Enabling super speed property
    Add support to enable super speed property for usb3_0 and usb3_1
    in all platforms
    
    Signed-off-by: Arshad Khan <arshad.khan@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- usbps/data/usbps.tcl
+++ usbps/data/usbps.tcl
@@ -16,7 +16,9 @@
     proc usbps_generate {drv_handle} {
         ps7_reset_handle $drv_handle CONFIG.C_USB_RESET CONFIG.usb-reset
         set proctype [get_hw_family]
+	set super_speed 0
         set node [get_node $drv_handle]
+	set peripheral [hsi get_cells -hier -filter {IP_NAME == zynq_ultra_ps_e}]
         set default_dts [set_drv_def_dts $drv_handle]
         if {[string match -nocase $proctype "zynq"] } {
         set_drv_prop $drv_handle phy_type ulpi $node string
@@ -31,13 +33,27 @@
 
         if {[string match -nocase $proctype "versal"] || [string match -nocase $proctype "psv_cortexr5"] || [string match -nocase $proctype "psv_pmc"]} {
                 #TODO:Remove this once the versal dts is fully updated.
+		add_prop $node "xlnx,enable-superspeed" $super_speed int $default_dts
                 return
         }
         if {[string match -nocase $mainline_ker "none"]} {
              set index [string index $drv_handle end]
              add_prop $node "status" "okay" string $default_dts
         }
+	if {$peripheral > 0} {
+		set usb3_0 0
+		set usb3_1 0
+		set parameters [hsi list_property [hsi get_cells -hier $peripheral]]
+		if {[lsearch -nocase $parameters "CONFIG.PSU__USB3_0__PERIPHERAL__ENABLE"] >= 0} {
+			set usb3_0 [hsi get_property CONFIG.PSU__USB3_0__PERIPHERAL__ENABLE [hsi get_cells -hier $peripheral]]
+		}
+		if {[lsearch -nocase $parameters "CONFIG.PSU__USB3_1__PERIPHERAL__ENABLE"] >= 0} {
+			set usb3_1 [hsi get_property CONFIG.PSU__USB3_1__PERIPHERAL__ENABLE [hsi get_cells -hier $peripheral]]
+		}
+		if { $usb3_0 || $usb3_1 } {
+			set super_speed 1
+		}
         }
     }
-
-
+    add_prop $node "xlnx,enable-superspeed" $super_speed int $default_dts
+    }
