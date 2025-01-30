ca771102529cbef8632a6c6d0d6647d7f938f513
Kunal Rane <kunal.rane@amd.com>
2025-01-30 00:08:32 -1200

    mipi_csi2_tx_ss: data: Update missing properties
    Add missing properties in the MIPI CSI2 TX SS tcl.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    Acked-by: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- mipi_csi2_tx_ss/data/mipi_csi2_tx_ss.tcl
+++ mipi_csi2_tx_ss/data/mipi_csi2_tx_ss.tcl
@@ -1,6 +1,6 @@
 #
 # (C) Copyright 2018-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -19,6 +19,28 @@ proc mipi_csi2_tx_ss_generate {drv_handle} {
         if {$node == 0} {
                 return
         }
+	set compatible [get_comp_str $drv_handle]
+
+	set tx_lanes [hsi get_property CONFIG.C_CSI_LANES [hsi::get_cells -hier $drv_handle]]
+        add_prop "${node}" "xlnx,csi2-tx-lanes" $tx_lanes int $dts_file 1
+
+	set cmn_pxl_format [hsi get_property CONFIG.C_CSI_DATATYPE [hsi::get_cells -hier $drv_handle]]
+	set cmn_pixel [mipi_csi2_tx_gen_pixel_format $cmn_pxl_format]
+        add_prop "${node}" "xlnx,csi2-tx-data-type" $cmn_pixel hexint $dts_file 1
+
+	set tx_pixel_mode [hsi get_property CONFIG.C_CSI_PIXEL_MODE [hsi::get_cells -hier $drv_handle]]
+        add_prop "${node}" "xlnx,csi2-tx-pixel-mode" $tx_pixel_mode int $dts_file 1
+
+	set tx_line_bufr_depth [hsi get_property CONFIG.C_CSI_LINE_BUFR_DEPTH [hsi::get_cells -hier $drv_handle]]
+        add_prop "${node}" "xlnx,csi2-tx-line-bufr-depth" $tx_line_bufr_depth int $dts_file 1
+
+	set dphy_linerate [hsi get_property CONFIG.C_DPHY_LINERATE [hsi::get_cells -hier $drv_handle]]
+        if  {[string match -nocase "true" $dphy_linerate]} {
+                add_prop "${node}" "xlnx,dphy-linerate" 1 int $dts_file 1
+        } elseif {[string match -nocase "false" $dphy_linerate]} {
+                add_prop "${node}" "xlnx,dphy-linerate" 0 int $dts_file 1
+	}
+
 	set dphy_en_reg_if [hsi get_property CONFIG.C_DPHY_EN_REG_IF [hsi::get_cells -hier $drv_handle]]
         if  {[string match -nocase "true" $dphy_en_reg_if]} {
                 add_prop "${node}" "xlnx,dphy-en-reg-if" 1 int $dts_file 1
@@ -38,7 +60,6 @@ proc mipi_csi2_tx_ss_generate {drv_handle} {
         } elseif {[string match -nocase "slave" $dphymode]} {
                 add_prop "${node}" "xlnx,dphy-mode" 0 int $dts_file 1
 	}
-
 
 	set highaddr [hsi get_property CONFIG.C_HIGHADDR  [hsi get_cells -hier $drv_handle]]
 	add_prop "${node}" "xlnx,highaddr" $highaddr hexint $dts_file 1
@@ -63,7 +84,7 @@ proc csitx2_add_hier_instances {drv_handle} {
 	#
 
 	set ip_subcores [dict create]
-	dict set ip_subcores "mipi_csi2_tx_ctrl" "csitx"
+	dict set ip_subcores "mipi_csi2_tx_ctrl" "csi2tx"
 	dict set ip_subcores "mipi_dphy" "dphy"
 #	dict set ip_subcores "hdcp22_rx" "hdcp22"
 	foreach ip [dict keys $ip_subcores] {
