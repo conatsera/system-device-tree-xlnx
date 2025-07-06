917cc1a9b9e43494301e7291ac285ca85c5d2c59
Bhavana Jupalli <Bhavana.Jupalli@amd.com>
2025-07-06 06:57:04 +0530

    xdmapcie: Add DT properties only when IP configured as Root Port
    Generate QDMA DT properties when IP is configured as a RootPort mode.
    
    Signed-off-by: Bhavana Jupalli <Bhavana.Jupalli@amd.com>
    Acked-by: Bharat Kumar Gogada <bharat.kumar.gogada@amd.com>

--- xdmapcie/data/xdmapcie.tcl
+++ xdmapcie/data/xdmapcie.tcl
@@ -13,10 +13,10 @@
 #
 
 proc xdmapcie_generate {drv_handle} {
-        set node [get_node $drv_handle]
-        if {$node == 0} {
-            return
-        }
+	set node [get_node $drv_handle]
+	if {$node == 0} {
+		return
+	}
 	set ip_type [hsi get_property IP_NAME $drv_handle]
 	set default_dts [set_drv_def_dts $drv_handle]
 	set val -1
@@ -24,80 +24,76 @@ proc xdmapcie_generate {drv_handle} {
 	if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
 		if {[string match *qdma* $ip_type]} {
 			set val [hsi get_property CONFIG.device_port_type $drv_handle]
+			pldt append $node compatible "\ \, \"xlnx,qdma-host-3.00\""
 		}
-		if {[string match -nocase $val "PCI_Express_Endpoint_device"]} {
-			add_prop $node "status" "disabled" string $default_dts 1
-		}
-
-		set axibar_num [get_ip_property $drv_handle "CONFIG.axibar_num"]
-		set range_type 0x02000000
-		# 64-bit high address.
-		set high_64bit 0x00000000
-		set ranges ""
-               set proctype [get_hw_family]
-                for {set x 0} {$x < $axibar_num} {incr x} {
-			if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
-				set axi_baseaddr [get_ip_property $drv_handle [format "CONFIG.axibar_%d" $x]]
-				set pcie_baseaddr [get_ip_property $drv_handle [format "CONFIG.axibar2pciebar_%d" $x]]
-				set axi_highaddr [get_ip_property $drv_handle [format "CONFIG.axibar_highaddr_%d" $x]]
-		}
-		set size [expr $axi_highaddr -$axi_baseaddr + 1]
-		# Check the size of pci memory region is 4GB or not,if
-		# yes then split the size to MSB and LSB.
-		if {[regexp -nocase {([0-9a-f]{9})} "$size" match]} {
-			set size [format 0x%016x [expr $axi_highaddr -$axi_baseaddr + 1]]
-			set low_size [string range $size 0 9]
-			set high_size "0x[string range $size 10 17]"
-			set size "$low_size $high_size"
-		} else {
-			set size [format 0x%08x [expr $axi_highaddr - $axi_baseaddr + 1]]
-			set size "$high_64bit $size"
-		}
-		if {[regexp -nocase {([0-9a-f]{9})} "$axi_baseaddr" match] || [regexp -nocase {([0-9a-f]{9})} "$axi_highaddr" match]} {
-			set range_type 0x43000000
-		}
-		if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
-			if {[regexp -nocase {([0-9a-f]{9})} "$pcie_baseaddr" match]} {
-				set temp $pcie_baseaddr
-				set temp [string trimleft [string trimleft $temp 0] x]
-				set len [string length $temp]
-				set rem [expr {${len} - 8}]
-				set high_base "0x[string range $temp $rem $len]"
-				set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
-				set low_base [format 0x%08x $low_base]
-				set pcie_baseaddr "$low_base $high_base"
-			} else {
-				set pcie_baseaddr "$high_64bit $pcie_baseaddr"
+		if {[string match -nocase $val "Root_Port_of_PCI_Express_Root_Complex"]} {
+			set axibar_num [get_ip_property $drv_handle "CONFIG.axibar_num"]
+			set range_type 0x02000000
+			# 64-bit high address.
+			set high_64bit 0x00000000
+			set ranges ""
+			set proctype [get_hw_family]
+			for {set x 0} {$x < $axibar_num} {incr x} {
+				if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
+					set axi_baseaddr [get_ip_property $drv_handle [format "CONFIG.axibar_%d" $x]]
+					set pcie_baseaddr [get_ip_property $drv_handle [format "CONFIG.axibar2pciebar_%d" $x]]
+					set axi_highaddr [get_ip_property $drv_handle [format "CONFIG.axibar_highaddr_%d" $x]]
+			        }
+				set size [expr $axi_highaddr -$axi_baseaddr + 1]
+				# Check the size of pci memory region is 4GB or not,if
+				# yes then split the size to MSB and LSB.
+				if {[regexp -nocase {([0-9a-f]{9})} "$size" match]} {
+					set size [format 0x%016x [expr $axi_highaddr -$axi_baseaddr + 1]]
+					set low_size [string range $size 0 9]
+					set high_size "0x[string range $size 10 17]"
+					set size "$low_size $high_size"
+				} else {
+					set size [format 0x%08x [expr $axi_highaddr - $axi_baseaddr + 1]]
+					set size "$high_64bit $size"
+				}
+				if {[regexp -nocase {([0-9a-f]{9})} "$axi_baseaddr" match] || [regexp -nocase {([0-9a-f]{9})} "$axi_highaddr" match]} {
+					set range_type 0x43000000
+				}
+				if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
+					if {[regexp -nocase {([0-9a-f]{9})} "$pcie_baseaddr" match]} {
+						set temp $pcie_baseaddr
+						set temp [string trimleft [string trimleft $temp 0] x]
+						set len [string length $temp]
+						set rem [expr {${len} - 8}]
+						set high_base "0x[string range $temp $rem $len]"
+						set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
+						set low_base [format 0x%08x $low_base]
+						set pcie_baseaddr "$low_base $high_base"
+					} else {
+						set pcie_baseaddr "$high_64bit $pcie_baseaddr"
+					}
+					if {[regexp -nocase {([0-9a-f]{9})} "$axi_baseaddr" match]} {
+						set temp $axi_baseaddr
+						set temp [string trimleft [string trimleft $temp 0] x]
+						set len [string length $temp]
+						set rem [expr {${len} - 8}]
+						set high_base "0x[string range $temp $rem $len]"
+						set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
+						set low_base [format 0x%08x $low_base]
+						set axi_baseaddr "$low_base $high_base"
+					} else {
+						set axi_baseaddr "0x0 $axi_baseaddr"
+					}
+					set value "$range_type $pcie_baseaddr $axi_baseaddr $size"
+				} else {
+					set value "$range_type $high_64bit $pcie_baseaddr $axi_baseaddr $size"
+				}
+				if {[string match "" $ranges]} {
+					set ranges $value
+				} else {
+					append ranges "> , <" $value
+				}
 			}
-			if {[regexp -nocase {([0-9a-f]{9})} "$axi_baseaddr" match]} {
-				set temp $axi_baseaddr
-				set temp [string trimleft [string trimleft $temp 0] x]
-				set len [string length $temp]
-				set rem [expr {${len} - 8}]
-				set high_base "0x[string range $temp $rem $len]"
-				set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
-				set low_base [format 0x%08x $low_base]
-				set axi_baseaddr "$low_base $high_base"
-			} else {
-				set axi_baseaddr "0x0 $axi_baseaddr"
-			}
-			set value "$range_type $pcie_baseaddr $axi_baseaddr $size"
-		} else {
-			set value "$range_type $high_64bit $pcie_baseaddr $axi_baseaddr $size"
+			add_prop $node "ranges" $ranges hexlist "pl.dtsi"
 		}
-		if {[string match "" $ranges]} {
-			set ranges $value
-		} else {
-			append ranges "> , <" $value
-		}
-	}
 
-	add_prop $node "ranges" $ranges hexlist "pl.dtsi"
-
-	}
-
-	if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
-		pldt append $node compatible "\ \, \"xlnx,qdma-host-3.00\""
+	if {[string match -nocase $val "Root_Port_of_PCI_Express_Root_Complex"]} {
+		add_prop $node "xlnx,device_port_type" "Root_Port_of_PCI_Express_Root_Complex" stringlist "pl.dtsi" 1
 		add_prop $node "xlnx,csr-slcr" 0x90000000 hexlist "pl.dtsi" 1
 		add_prop $node "xlnx,num-of-bars" 0x2 hexint "pl.dtsi" 1
 		add_prop $node "xlnx,include-baroffset-reg" 0x1 hexint "pl.dtsi" 1
@@ -123,5 +119,8 @@ proc xdmapcie_generate {drv_handle} {
 		add_prop "${pcie_child_intc_node}" "interrupt-controller" boolean "pl.dtsi"
 		add_prop "${pcie_child_intc_node}" "#address-cells" 0 int "pl.dtsi"
 		add_prop "${pcie_child_intc_node}" "#interrupt-cells" 1 int "pl.dtsi"
-        }
+	} else {
+		add_prop $node "xlnx,device_port_type" "PCI_Express_Endpoint_device" stringlist "pl.dtsi" 1
+	}
+    }
 }
