6a0c97ffbbfca17b3101b378d83dc15d96dae800
Onkar Harsh <onkar.harsh@amd.com>
2025-07-11 14:16:50 +0530

    common: Add support for small variants of Versal 2VE and 2VM SOC family
    In some of the small variants of Versal 2VE and 2VM SOC family, the PS11 and MMI
    sub-cores of Wizard is combined into a new block PSXT. Add support for the same.
    
    In addition, make the logic to get the number of RPUs in the design generic.
    Remove the platform specific hard coding of number of RPU cores.
    
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- device_tree/data/device_tree.tcl
+++ device_tree/data/device_tree.tcl
@@ -934,7 +934,7 @@ proc get_sem_property { prop } {
 		set pspmcCell [hsi::get_cells -hier -filter "IP_NAME==psxl"]
 	}
 	if {$pspmcCell eq ""} {
-		set pspmcCell [hsi::get_cells -hier -filter {IP_NAME==ps11 || IP_NAME==ps11xgui}]
+		set pspmcCell [hsi::get_cells -hier -filter {IP_NAME==ps11 || IP_NAME==ps11xgui || IP_NAME==psxt}]
 	}
 	if {$pspmcCell ne ""} {
 		set readconfig [common::get_property $prop $pspmcCell]
@@ -1627,7 +1627,7 @@ Generates system device tree based on args given in:
 		}
 	}
 
-	set non_val_list "versal_cips psx_wizard psxl ps_wizard ps11 ps11xgui dmac_slv axi_noc axi_noc2 noc_mc_ddr4 noc_mc_ddr5 ddr3 ddr4 mig_7series hbm noc_nmu noc_nsu noc2_nmu noc2_nsu ila zynq_ultra_ps_e psu_iou_s smart_connect emb_mem_gen xlconcat xlconstant xlslice axis_tdest_editor util_reduced_logic noc_nsw noc2_nsw axis_ila pspmc pmcps psv_ocm_ram_0 ocm_ram psv_pmc_qspi_ospi psx_pmc_qspi_ospi pmc_qspi_ospi add_keep_128 c_counter_binary dbg_monmux tsn_endpoint_ethernet_mac_block ${linear_spi_list}"
+	set non_val_list "versal_cips psx_wizard psxl ps_wizard ps11 ps11xgui psxt dmac_slv axi_noc axi_noc2 noc_mc_ddr4 noc_mc_ddr5 ddr3 ddr4 mig_7series hbm noc_nmu noc_nsu noc2_nmu noc2_nsu ila zynq_ultra_ps_e psu_iou_s smart_connect emb_mem_gen xlconcat xlconstant xlslice axis_tdest_editor util_reduced_logic noc_nsw noc2_nsw axis_ila pspmc pmcps psv_ocm_ram_0 ocm_ram psv_pmc_qspi_ospi psx_pmc_qspi_ospi pmc_qspi_ospi add_keep_128 c_counter_binary dbg_monmux tsn_endpoint_ethernet_mac_block ${linear_spi_list}"
 	set non_val_ip_types "MONITOR BUS PROCESSOR"
 	set non_val_list1 "psv_cortexa72 psu_cortexa53 ps7_cortexa9 versal_cips psx_wizard ps_wizard noc_nmu noc_nsu ila psu_iou_s noc_nsw pspmc pmcps"
 	set non_val_ip_types1 "MONITOR BUS"
@@ -2370,10 +2370,11 @@ proc gen_cpu_cluster {} {
     	if { $r5_present } {
     		set r5_cores 2
     		if { $is_versal_net_platform } {
-    			set r5_cores 4
+			set rpu_ip_name "psx_cortexr52"
 			if { $is_versal_2ve_2vm_platform } {
-				set r5_cores 10
+				set rpu_ip_name "cortexr52"
 			}
+			set r5_cores [llength [hsi get_cells -hier -filter IP_NAME==$rpu_ip_name]]
     		}
 		for {set core 0} {$core < $r5_cores} {incr core} {
 			set proc_name "r5"
