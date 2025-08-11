0de6ca3e67aa8eb52d056f03abe3ef144ca48301
Rajesh Gugulothu <rajesh.gugulothu@amd.com>
2025-08-11 18:54:59 +0530

    v_hdmi_tx_ss: Subcore absolute address calculation
    When multiple instances of HDMI TX subsystems exist in a design,
    subcore address are generated as same in their respective g.c files
    which may call only first configptr always. To resolve this issue,
    subcore address is calculated and provided its absolute address
    in their g.c files instead of its base offset.
    
    Signed-off-by: Rajesh Gugulothu <rajesh.gugulothu@amd.com>
    Acked-by: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- hdmi_tx_ss/data/hdmi_tx_ss.tcl
+++ hdmi_tx_ss/data/hdmi_tx_ss.tcl
@@ -13,6 +13,8 @@
 # GNU General Public License for more details.
 #
 
+source [file join [file dirname [info script]] "../../device_tree/data/video_utils.tcl"]
+
 proc hdmi_tx_ss_generate {drv_handle} {
 	set node [get_node $drv_handle]
 	set dts_file [set_drv_def_dts $drv_handle]
@@ -278,6 +280,7 @@ proc hdmitxss_add_hier_instances {drv_handle} {
 		if {![string_is_empty $ip_handle]} {
 			add_prop "$node" "${ip_prefix}-present" 1 int $dts_file
 			add_prop "$node" "${ip_prefix}-connected" $ip_handle reference $dts_file
+			update_subcore_absolute_addr $drv_handle $ip_handle $dts_file
 		} else {
 			add_prop "$node" "${ip_prefix}-present" 0 int $dts_file
 		}
@@ -292,6 +295,7 @@ proc hdmitxss_add_hier_instances {drv_handle} {
 			if { [regexp -nocase $drv_handle $timer match] } {
 				add_prop "$node" "hdcptimer-present" 1 int $dts_file
 				add_prop "$node" "hdcptimer-connected" $timer reference $dts_file
+				update_subcore_absolute_addr $drv_handle $timer $dts_file
 			} else {
 				add_prop "$node" "hdcptimer-present" 0 int $dts_file
 			}
