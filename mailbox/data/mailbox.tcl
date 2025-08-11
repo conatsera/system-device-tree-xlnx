d03ebc5d80910b3f5a5d5e3fb448c763adc2f401
T, Harini <Harini.T@amd.com>
2025-08-11 11:26:40 +0530

    mailbox: Refactor mailbox node generation by optimizing processor loop
    - Instead of creating 2 mailbox nodes for all processors and then
      deleting based on processor mapping, create 2 mailbox nodes for each
      mailbox IP instance and loop each node through all processors and map
      only if it is connected to the processor.
    - Simplify check_if_connected proc to focus only on AXI4-LITE
      connectivity.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- mailbox/data/mailbox.tcl
+++ mailbox/data/mailbox.tcl
@@ -19,8 +19,6 @@ proc mailbox_generate {drv_handle} {
 	}
 
 	#Obtain interrupt values
-	set intr_val ""
-	set intr_parent ""
 	if {[catch {
 		set intr_val [pldt get $node interrupts]
 		set intr_parent [pldt get $node interrupt-parent]
@@ -38,19 +36,15 @@ proc mailbox_generate {drv_handle} {
 	#Delete the Mailbox Core node
 	pldt delete $node
 
-	set proclist [hsi::get_cells -hier -filter IP_TYPE==PROCESSOR]
-
 	#Create 2 Mailbox nodes for each Mailbox IP
-	foreach processor $proclist {
-		for {set port_id 0} {$port_id < 2} {incr port_id} {
-			# port_interface: 2 = AXI4-Lite, 4 = AXI4-Stream
-			set port_interface [common::get_property CONFIG.[format "C_INTERCONNECT_PORT_%d" $port_id] $drv_handle]
-			create_mbox_nodes $drv_handle $port_interface $port_id $processor $intr_val $intr_parent
-		}
+	for {set port_id 0} {$port_id < 2} {incr port_id} {
+		# port_interface: 2 = AXI4-Lite, 4 = AXI4-Stream
+		set port_interface [common::get_property CONFIG.[format "C_INTERCONNECT_PORT_%d" $port_id] $drv_handle]
+		create_mbox_nodes $drv_handle $port_interface $port_id $intr_val $intr_parent
 	}
 }
 
-proc create_mbox_nodes {drv_handle port_interface port_id processor intr_val intr_parent} {
+proc create_mbox_nodes {drv_handle port_interface port_id intr_val intr_parent} {
 
 	#Identify if the processor is 32-bit or 64-bit
 	set family [get_hw_family]
@@ -79,98 +73,83 @@ proc create_mbox_nodes {drv_handle port_interface port_id processor intr_val int
 	set mbox_delete_node	0
 
 	set periph_name [string toupper [common::get_property NAME $drv_handle]]
+	set proclist [hsi::get_cells -hier -filter IP_TYPE==PROCESSOR]
+	foreach processor $proclist {
+		if {$port_interface == 2} {
+			#AXI4LITE interface
+			set mbox_baseaddr [common::get_property CONFIG.[format "C_S%d_AXI_BASEADDR" $port_id] $drv_handle]
+			set mbox_highaddr [common::get_property CONFIG.[format "C_S%d_AXI_HIGHADDR" $port_id] $drv_handle]
+			set is_axi4lite_connected [check_if_connected $drv_handle $port_id $port_interface $processor]
+		} else {
+			#AXI4STREAM interface
+			set send_fsl 0
+			set recv_fsl  0
+			set delete_node	0
 
-	if {$port_interface == 2} {
-		#AXI4LITE interface
-		set mbox_baseaddr [common::get_property CONFIG.[format "C_S%d_AXI_BASEADDR" $port_id] $drv_handle]
-		set mbox_highaddr [common::get_property CONFIG.[format "C_S%d_AXI_HIGHADDR" $port_id] $drv_handle]
-		set is_axi4lite_connected [check_if_connected $drv_handle $port_id $port_interface $processor]
-	} else {
-		#AXI4STREAM interface
-		set send_fsl 0
-		set recv_fsl  0
-		set delete_node	0
+			handle_stream $drv_handle $port_interface $port_id $processor use_fsl send_fsl recv_fsl delete_node
+			set is_axi4lite_connected	0
 
-		handle_stream $drv_handle $port_interface $port_id $processor use_fsl send_fsl recv_fsl delete_node
-		set is_axi4lite_connected	0
+			set mbox_use_fsl        $use_fsl
+			set mbox_send_fsl       $send_fsl
+			set mbox_recv_fsl       $recv_fsl
 
-		set mbox_use_fsl        $use_fsl
-		set mbox_send_fsl       $send_fsl
-		set mbox_recv_fsl       $recv_fsl
-
-		set mbox_delete_node	$delete_node
-	}
-
-	if {$mbox_baseaddr != 0} {
-		set nodename_baseaddr [format %lx $mbox_baseaddr]
-	} else {
-		set mbox_baseaddr 0x0
-		set mbox_highaddr 0xFFFF
-		set nodename_baseaddr 0
-	}
-
-	set node [create_node -n $label_name -l $label_name -u $nodename_baseaddr -p $bus_name -d $dts_file]
-	set reg [gen_reg_property_format $mbox_baseaddr $mbox_highaddr $bit_format]
-
-	if { $is_axi4lite_connected == 1 || $use_fsl == 1 } {
-		#Add properties to the node
-		add_prop "${node}" "xlnx,send-fsl" $mbox_send_fsl int $dts_file
-		add_prop "${node}" "xlnx,recv-fsl" $mbox_recv_fsl int $dts_file
-		add_prop "${node}" "xlnx,use-fsl" $mbox_use_fsl int $dts_file
-		add_prop "${node}" "compatible" $compatible string $dts_file
-		add_prop "${node}" "xlnx,ip-name" $ip_name string $dts_file
-		add_prop "${node}" "xlnx,name" $name string $dts_file
-		add_prop "${node}" "status" "okay" string $dts_file
-		add_prop "${node}" "reg" $reg hexlist $dts_file
-
-		#Append generic compatible string
-		pldt append $node compatible "\ \, \"xlnx,mailbox\""
-
-		#Add interrupt properties
-		if {![string_is_empty $intr_val]} {
-			add_prop "${node}" "interrupts" $intr_val intlist $dts_file
-		}
-		if {![string_is_empty $intr_parent]} {
-			add_prop "${node}" "interrupt-parent" $intr_parent reference  $dts_file
+			set mbox_delete_node	$delete_node
 		}
 
-		#Processor mapping
-		map_node_to_processor "${label_name}" $processor $reg $bit_format $mbox_baseaddr $size
-	}
+		if {$mbox_baseaddr != 0} {
+			set nodename_baseaddr [format %lx $mbox_baseaddr]
+		} else {
+			set mbox_baseaddr 0x0
+			set mbox_highaddr 0xFFFF
+			set nodename_baseaddr 0
+		}
 
-	#Delete node if C_USE_EXTENDED_FSL_INSTR not enabled on Microblaze for AXI Stream interface connectivity with mailbox core
-	if { $mbox_delete_node == 1 } {
-		pldt delete $node
+		set node [create_node -n $label_name -l $label_name -u $nodename_baseaddr -p $bus_name -d $dts_file]
+		set reg [gen_reg_property_format $mbox_baseaddr $mbox_highaddr $bit_format]
+
+		if { $is_axi4lite_connected == 1 || $use_fsl == 1 } {
+			#Add properties to the node
+			add_prop "${node}" "xlnx,send-fsl" $mbox_send_fsl int $dts_file
+			add_prop "${node}" "xlnx,recv-fsl" $mbox_recv_fsl int $dts_file
+			add_prop "${node}" "xlnx,use-fsl" $mbox_use_fsl int $dts_file
+			add_prop "${node}" "compatible" $compatible string $dts_file
+			add_prop "${node}" "xlnx,ip-name" $ip_name string $dts_file
+			add_prop "${node}" "xlnx,name" $name string $dts_file
+			add_prop "${node}" "status" "okay" string $dts_file
+			add_prop "${node}" "reg" $reg hexlist $dts_file
+
+			#Append generic compatible string
+			pldt append $node compatible "\ \, \"xlnx,mailbox\""
+
+			#Add interrupt properties
+			if {![string_is_empty $intr_val]} {
+				add_prop "${node}" "interrupts" $intr_val intlist $dts_file
+			}
+			if {![string_is_empty $intr_parent]} {
+				add_prop "${node}" "interrupt-parent" $intr_parent reference  $dts_file
+			}
+
+			#Processor mapping
+			map_node_to_processor "${label_name}" $processor $reg $bit_format $mbox_baseaddr $size
+		}
+
+		#Delete node if C_USE_EXTENDED_FSL_INSTR not enabled on Microblaze for AXI Stream interface connectivity with mailbox core
+		if { $mbox_delete_node == 1 } {
+			pldt delete $node
+		}
 	}
 }
 
 proc check_if_connected {periph port_id port_interface processor} {
 	set is_axi4lite_connected 0
-	set if_axistream_connected 0
 
-	if {$port_interface == 2} {
-		#Check if AXI4LITE interface is connected.
-		set baseaddr [common::get_property CONFIG.[format "C_S%d_AXI_BASEADDR" $port_id] $periph]
-		set mem [hsi::get_mem_ranges -of_objects [hsi::get_cells -hier $processor] -filter INSTANCE==$periph]
-		if {[llength [lindex $mem 0]] != 0} {
-			set addrs [common::get_property BASE_VALUE $mem]
-			if {$addrs == $baseaddr} {
-				set is_axi4lite_connected 1
-			}
+	set baseaddr [common::get_property CONFIG.[format "C_S%d_AXI_BASEADDR" $port_id] $periph]
+	set mem [hsi::get_mem_ranges -of_objects [hsi::get_cells -hier $processor] -filter INSTANCE==$periph]
+	if {[llength [lindex $mem 0]] != 0} {
+		set addrs [common::get_property BASE_VALUE $mem]
+		if {$addrs == $baseaddr} {
+			set is_axi4lite_connected 1
 		}
-	} else {
-		set delete_node	0
-		set recv_fsl  0
-		set send_fsl 0
-		set use_fsl 0
-
-		handle_stream $periph $bus_if $if_num $processor use_fsl send_fsl recv_fsl delete_node
-		set if_axistream_connected $use_fsl
-	}
-	if {$is_axi4lite_connected || $if_axistream_connected} {
-		return 1
-	} else {
-		return 0
 	}
 }
 
